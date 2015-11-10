a8a386e11d1954216f12224f1952272f793fe0d7
Zhongmiao Li <mars.leezm@gmail.com>
2015-11-10 16:19:59 +0000

    Add calling by name

--- src/tx_cert_sup.erl
+++ src/tx_cert_sup.erl
@@ -26,16 +26,47 @@
 
 -export([start_link/0]).
 
--export([init/1, certify/4]).
+-export([init/1, certify/4, get_stat/1, get_internal_data/3, read/4]).
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-certify(ThreadId, TxId, LocalUpdates, RemoteUpdates) ->
-    gen_server:call(generate_module_name(ThreadId rem ?NUM_SUP), {certify, TxId, LocalUpdates, RemoteUpdates}).
+certify(Name, TxId, LocalUpdates, RemoteUpdates) ->
+    case is_integer(Name) of
+        true ->
+            gen_server:call({global, generate_module_name(Name rem ?NUM_SUP)}, 
+                    {certify, TxId, LocalUpdates, RemoteUpdates});
+        false ->
+            gen_server:call({global, Name}, 
+                    {certify, TxId, LocalUpdates, RemoteUpdates})
+    end.
+
+get_internal_data(Name, Type, Param) ->
+    case is_integer(Name) of
+        true ->
+            gen_server:call({global, generate_module_name(Name rem ?NUM_SUP)}, {get_internal_data, Type, Param});
+        false ->
+            gen_server:call({global, Name}, {get_internal_data, Type, Param})
+    end.
+
+read(Name, TxId, Key, Node) ->
+    case is_integer(Name) of
+        true ->
+            gen_server:call({global, generate_module_name(Name rem ?NUM_SUP)}, {read, Key, TxId, Node});
+        false ->
+            gen_server:call({global, Name}, {read, Key, TxId, Node})
+    end.
+
+get_stat(Name) ->
+    case is_integer(Name) of
+        true ->
+            gen_server:call({global, generate_module_name(Name rem ?NUM_SUP)}, {get_stat});
+        false ->
+            gen_server:call({global, Name}, {get_stat})
+    end.
 
 generate_module_name(N) ->
-    list_to_atom(atom_to_list(?MODULE) ++ "-" ++ integer_to_list(N)).
+    list_to_atom(atom_to_list(node()) ++ "-cert-" ++ integer_to_list(N)).
 
 generate_supervisor_spec(N) ->
     Module = generate_module_name(N),
