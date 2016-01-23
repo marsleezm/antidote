e52b50cb0e251f6704ba9a20484bab06754af6a7
Zhongmiao Li <mars.leezm@gmail.com>
2016-01-23 19:31:37 +0000

    dONE

--- src/specula_tx_cert_server.erl
+++ src/specula_tx_cert_server.erl
@@ -207,7 +207,6 @@ handle_call({certify, TxId, LocalUpdates, RemoteUpdates},  Sender, SD0=#state{re
             num_specula_read=NumSpeculaRead}) ->
     %RemoteKeys = lists:map(fun({Node, Ups}) -> {Node, [Key || {Key, _} <- Ups]} end, RemoteUpdates),
     %% If there was a legacy ongoing transaction.
-     lager:warning("Got req: txid ~w, localUpdates ~p, remote updates ~p", [TxId, LocalUpdates, RemoteUpdates]),
      lager:warning("Got req: txid ~w", [TxId]),
     ReadDepTxs = [T2  || {_, T2} <- ets:lookup(anti_dep, TxId)],
     true = ets:delete(anti_dep, TxId),
