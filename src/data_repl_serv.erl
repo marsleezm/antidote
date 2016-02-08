e69f1825a8156ad7545f81542e5f7ef04f23ee69
Zhongmiao Li <mars.leezm@gmail.com>
2016-02-08 18:04:38 +0100

    Add data repl

--- src/data_repl_serv.erl
+++ src/data_repl_serv.erl
@@ -166,7 +166,7 @@ init([Name, _Parts]) ->
     %            dict:store(Part, 0, Acc) end, dict:new(), Parts),
     %lager:info("Parts are ~w, TsDict is ~w", [Parts, dict:to_list(TsDict)]),
     %lager:info("Concurrent is ~w, num partitions are ~w", [Concurrent, NumPartitions]),
-    {ok, #state{name=Name, set_size= NumPartitions*Concurrent,
+    {ok, #state{name=Name, set_size= max(NumPartitions*Concurrent, 40),
                 pending_log = PendingLog, current_dict = dict:new(), ts_dict=dict:new(), do_specula=DoSpecula,
                 backup_dict = dict:new(), replicated_log = ReplicatedLog}}.
 
