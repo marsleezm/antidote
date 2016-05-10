24cf9d44dae86dfa30e815b1778b2039f10ba083
Zhongmiao Li <mars.leezm@gmail.com>
2016-05-10 21:46:43 +0200

    Commit antidote pb

--- src/antidote_app.erl
+++ src/antidote_app.erl
@@ -25,7 +25,7 @@
 -export([start/2, stop/1]).
 
 %% PB Services
--define(SERVICES, [{antidote_pb_txn, 84, 100}]).
+%-define(SERVICES, [{antidote_pb_txn, 84, 100}]).
 
 %% ===================================================================
 %% Application callbacks
@@ -45,7 +45,7 @@ start(_StartType, _StartArgs) ->
 
             ok = riak_core_ring_events:add_guarded_handler(antidote_ring_event_handler, []),
             ok = riak_core_node_watcher_events:add_guarded_handler(antidote_node_event_handler, []),
-            ok = riak_api_pb_service:register(?SERVICES),
+            %ok = riak_api_pb_service:register(?SERVICES),
             {ok, Pid};
         {error, Reason} ->
             {error, Reason}
