cf6e1ec391d8151a2d8009840a970df124030de8
Zhongmiao Li <mars.leezm@gmail.com>
2016-07-19 05:16:58 +0200

    Revert "Increase size"
    This reverts commit a15d617a195c8be25c57373ff8b3ae423177fc97.

--- src/data_repl_serv.erl
+++ src/data_repl_serv.erl
@@ -170,7 +170,7 @@ init([Name, _Parts]) ->
     %            dict:store(Part, 0, Acc) end, dict:new(), Parts),
     %lager:info("Parts are ~w, TsDict is ~w", [Parts, dict:to_list(TsDict)]),
     %lager:info("Concurrent is ~w, num partitions are ~w", [Concurrent, NumPartitions]),
-    {ok, #state{name=Name, set_size= min(max(TotalReplFactor*8*Concurrent*max(SpeculaLength,2), 400), 30000), specula_read=SpeculaRead,
+    {ok, #state{name=Name, set_size= min(max(TotalReplFactor*8*Concurrent*max(SpeculaLength,1), 400), 25000), specula_read=SpeculaRead,
                 pending_log = PendingLog, current_dict = dict:new(), 
                 backup_dict = dict:new(), replicated_log = ReplicatedLog}}.
 
