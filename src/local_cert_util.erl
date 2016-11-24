04b3c09da46ff7038d0ce4972ee2cbbe4d254fff
Zhongmiao Li <mars.leezm@gmail.com>
2016-11-24 19:15:01 +0000

    Adding debuging

--- src/local_cert_util.erl
+++ src/local_cert_util.erl
@@ -173,7 +173,6 @@ check_prepared(TxId, PreparedTxs, Key, _Value) ->
         [{Key, LastReaderTime}] ->
             %ToPrepTime = max(LastReaderTime+1, PPTime),
             %ets:insert(PreparedTxs, {Key, [{TxId, ToPrepTime, ToPrepTime, PPTime, Value, []}]}),
-             lager:warning("LastReaderTime is ~w", [LastReaderTime]),
             {true, LastReaderTime+1}
     end.
 
@@ -853,14 +852,11 @@ insert_prepare(PreparedTxs, TxId, Partition, WriteSet, TimeStamp, Sender) ->
               lists:foreach(fun({Key, Value}) ->
                           case ets:lookup(PreparedTxs, Key) of
                               [] ->
-                                  lager:warning("Inserted prepare for ~w ~p having nothing", [TxId, Key]),
                                   true = ets:insert(PreparedTxs, {Key, [{repl_prepare, TxId, ToPrepTS, ToPrepTS, ToPrepTS, 1, Value, []}]});
                               [{Key, [{_, _, _, _, _, _, _, _}|_Rest]=L}] ->
                                   NewList = add_to_list(TxId, ToPrepTS, Value, L),
-                                  lager:warning("Key:~w, PrevList is ~w, after list is ~w", [Key, L, NewList]),
                                   true = ets:insert(PreparedTxs, {Key, NewList});
                               [{Key, _}] -> 
-                                  lager:warning("Inserted prepare for ~w ~p having nothing", [TxId, Key]),
                                   true = ets:insert(PreparedTxs, {Key, [{repl_prepare, TxId, ToPrepTS, ToPrepTS, ToPrepTS, 1, Value, []}]})
               end end,  WriteSet),
               lager:warning("Got repl prepare for ~p, propose ~p and replied", [TxId, ToPrepTS]),
