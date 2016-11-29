ab90b1268299d41350d535bd63bb1c9f3ddf83ae
Zhongmiao Li <mars.leezm@gmail.com>
2016-11-29 23:47:28 +0000

    Add hitcounter

--- src/i_tx_cert_server.erl
+++ src/i_tx_cert_server.erl
@@ -100,6 +100,9 @@ handle_call({get_cdf}, _Sender, SD0) ->
     %{reply, ok, SD0};
     {reply, ok, SD0};
 
+handle_call({get_hitcounter}, _Sender, SD0) ->
+    {reply, 0, SD0};
+
 handle_call({get_stat}, _Sender, SD0) ->
     {reply, [0, 0, 0, 0, 0, 0, 0], SD0};
 
